11e86a281a2787ef7dfc75ac84b17cc3bc1b6fb2
quinten-bosch <qpro1234gamer@gmail.com>
2021-02-16 14:43:07 +0100

    Added logic

---
+++ Labo 3 GET en Selenium/src/main/java/ui/controller/Servlet.java
@@ -0,0 +1,38 @@
+package ui.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/StudentInfo")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<head>");
+
+
+        out.println("<body>");
+
+
+        out.println("<p id='dynamisch'>");
+        out.println("Naam:");
+        String naam = request.getParameter("naam");
+        String voornaam = request.getParameter("voornaam");
+        out.println(naam);
+        out.println(voornaam);
+        out.println("</p>");
+        out.println("</body>");
+
+
+        out.println("</head>");
+    }
+}
