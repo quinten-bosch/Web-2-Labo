2018fcc10fafa1adda4762ff92823b06791c7891
quinten-bosch <qpro1234gamer@gmail.com>
2021-02-16 15:35:26 +0100

    Updated classes

--- Labo 3 GET en Selenium/src/main/java/domain/db/StudentDB.java
+++ Labo 3 GET en Selenium/src/main/java/domain/db/StudentDB.java
@@ -1,4 +1,23 @@
 package domain.db;
 
+import domain.model.Student;
+
+import java.util.ArrayList;
+
 public class StudentDB {
+    public static ArrayList<Student> students;
+    public StudentDB(){
+        students = new ArrayList<>();
+        Student greetje = new Student("Jongen", "Greetje", "Toegepaste Informatica", 23);
+        Student kristien = new Student("Melaerts", "Kristien", "Chemie", 21);
+        Student elke = new Student("Steegmans", "Elke", "Vroedkunde", 16);
+        Student jan = new Student("Van Hee", "Jan", "Verpleegkunde", 18);
+        students.add(greetje);
+        students.add(kristien);
+        students.add(elke);
+        students.add(jan);
+
+    }
+
+
 }
--- Labo 3 GET en Selenium/src/main/java/ui/controller/Servlet.java
+++ Labo 3 GET en Selenium/src/main/java/ui/controller/Servlet.java
@@ -1,5 +1,8 @@
 package ui.controller;
 
+import domain.db.StudentDB;
+import domain.model.Student;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +18,7 @@ public class Servlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
         out.println("<head>");
@@ -27,6 +31,9 @@ public class Servlet extends HttpServlet {
         out.println("Naam:");
         String naam = request.getParameter("naam");
         String voornaam = request.getParameter("voornaam");
+
+
+
         out.println(naam);
         out.println(voornaam);
         out.println("</p>");
